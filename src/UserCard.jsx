6ae26a9168eaee985d4dd5b4705ec3607bfd237c
Surya <129343874+surya9344@users.noreply.github.com>
2024-07-27 16:51:28 +0530

    Update UserCard.jsx

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -6,7 +6,7 @@ const userData=[
         city:"Chennai",
         about:"WEB DEVELOPER",
         online:false,
-        profile: "https://www.google.com/imgres?q=profile%20image%20avatar&imgurl=https%3A%2F%2Fpng.pngtree.com%2Fpng-clipart%2F20230927%2Foriginal%2Fpngtree-man-avatar-image-for-profile-png-image_13001882.png&imgrefurl=https%3A%2F%2Fpngtree.com%2Ffreepng%2Fman-avatar-image-for-profile_13001882.html&docid=jg1keOQMt--4lM&tbnid=gq4PHU4u5hkkhM&vet=12ahUKEwiZt___i8eHAxW-RmwGHRnWHLEQM3oECFIQAA..i&w=1200&h=1200&hcb=2&ved=2ahUKEwiZt___i8eHAxW-RmwGHRnWHLEQM3oECFIQAA",
+        profile: "src/assets/card1.jpg",
         skills:['HTML','CSS','JavaScript','React.js','C','JAVA','API','POSTMAN']
     },
     {
@@ -14,7 +14,7 @@ const userData=[
         city:"New York",
         about:"WEB DEVELOPER",
         online:true,
-        profile: "/src/assets/card2.jpg",
+        profile: "src/assets/card2.jpg",
         skills:['HTML','CSS','JavaScript','React.js','C','JAVA','Python','Postgresql','API','MongoDB','POSTMAN']
     },
     {
@@ -22,7 +22,7 @@ const userData=[
         city:"Phoenix",
         about:"SOFTWARE DEVELOPER",
         online:true,
-        profile: "/src/assets/card6.jpeg",
+        profile: "src/assets/card6.jpeg",
         skills:['React Native','Kotlin','HTML','CSS','JavaScript','React.js','C','JAVA','API','Python']
     }
 ];
